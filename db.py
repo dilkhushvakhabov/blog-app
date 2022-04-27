85ad2d0060d440d85a815c294c72414adc1b9bce
NozimbekJuraev <nozimjuraev1699@gmail.com>
2022-04-27 09:30:07 +0500

    add db

--- db.py
+++ db.py
@@ -5,7 +5,7 @@ import psycopg2
 
 def get_posts():
     try:
-        conn = psycopg2.connect(dbname="blog", user="postgres", password="123", host='localhost', port=5432)
+        conn = psycopg2.connect(dbname="blog_app", user="postgres", password="123", host='localhost', port=5432)
         cursor = conn.cursor()
         cursor.execute('SELECT * FROM posts')
         
