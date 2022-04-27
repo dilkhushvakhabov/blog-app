42d5821f061af3657efbda43997d81b64ca596bc
Dilkhush Vakhabov <dilkhushvakhabov@gmail.com>
2022-04-27 09:27:19 +0500

    add botstrapp and detail post

--- db.py
+++ db.py
@@ -17,3 +17,25 @@ def get_posts():
         return [{'id': post[0], 'title': post[1], 'content': post[2]} for post in posts]
     except Exception as e:
         print('DB ERROR')
+
+
+
+def get_post(pk):
+    try:
+        conn = psycopg2.connect(dbname="blog", user="postgres", password="123", host='localhost', port=5432)
+        cursor = conn.cursor()
+        cursor.execute('SELECT * FROM posts WHERE id=%d' % pk)
+        
+        post_qs = cursor.fetchone()
+        
+        cursor.close()
+        conn.close()
+        
+        return {
+            'id': post_qs[0],
+            'title': post_qs[1],
+            'content': post_qs[2]
+        }
+        
+    except Exception as e:
+        print('DB ERROR')
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-from db import get_posts
+from db import get_posts, get_post
 
 app = Flask(__name__)
 
@@ -10,7 +10,8 @@ def index_view():
     return render_template('index.html', ppp=get_posts(), name='Aziz')
 
 
-@app.route('/<int:pk>')
+@app.route('/<int:pk>/')
 def post_detail_view(pk):
-    return render_template('post_detail.html')
+    post = get_post(pk)
+    return render_template('post_detail.html', post=post)
 
